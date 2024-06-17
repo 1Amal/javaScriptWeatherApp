6623bb0821e57ee2b0477d2a722c7e60841e6254
Amal K <12756149+1Amal@users.noreply.github.com>
2024-06-17 12:22:34 +1000

    Added code to display weather data for day after tommorrow

--- src/displayController.js
+++ src/displayController.js
@@ -9,6 +9,8 @@ export class displayController {
 
   updateDisplay(weatherData) {
     console.log(weatherData);
+
+    const apiStatus=document.querySelector(".apiStatus");
     // Following code will select the relevant HTML Selectors for Day 1
     const displayLocation = document.querySelector(".displayLocation");
     const displayRegion = document.querySelector(".displayRegion");
@@ -57,6 +59,28 @@ export class displayController {
     const displaySunriseDay2 = document.querySelector(".displaySunriseDay2");
     const displaySunsetDay2 = document.querySelector(".displaySunsetDay2");
 
+    // Following code will select the relevant HTML Selectors for Day 3
+    const displayDate3 = document.querySelector(".displayDate3");
+    const DisplayTempAverageDay3 = document.querySelector(
+      ".DisplayTempAverageDay3"
+    );
+    const displayTempLowDay3 = document.querySelector(".displayTempLowDay3");
+    const displayTempHighDay3 = document.querySelector(".displayTempHighDay3");
+    const displayWeatherConditionDay3 = document.querySelector(
+      ".displayWeatherConditionDay3"
+    );
+    const displayHumidityDay3 = document.querySelector(".displayHumidityDay3");
+    const displayWindSpeedDay3 = document.querySelector(
+      ".displayWindSpeedDay3"
+    );
+
+    const displayUVDay3 = document.querySelector(".displayUVDay3");
+    const displaySunriseDay3 = document.querySelector(".displaySunriseDay3");
+    const displaySunsetDay3 = document.querySelector(".displaySunsetDay3");
+
+//Code to display API Status
+apiStatus.innerText="Display API Status Here";
+
     //Following code will update the above selected HTML selectors with relevant data for Day 1;
     displayLocation.innerText = weatherData.locationdDetails.location;
     displayRegion.innerText = weatherData.locationdDetails.region;
@@ -91,9 +115,26 @@ export class displayController {
       weatherData.weatherTommorrow.humidityAverage;
     displayWindSpeedDay2.innerText =
       weatherData.weatherTommorrow.windSpeedMaximum + " Km/h";
-
     displayUVDay2.innerText = weatherData.weatherTommorrow.uv;
     displaySunriseDay2.innerText = weatherData.weatherTommorrow.sunrise;
     displaySunsetDay2.innerText = weatherData.weatherTommorrow.sunset;
+
+    //Following code will update the above selected HTML selectors with relevant data for Day 3;
+    displayDate3.innerText = weatherData.weatherDayAfterTommorrow.currentDate;
+    DisplayTempAverageDay3.innerText =
+      weatherData.weatherDayAfterTommorrow.temperatureCAverage + "°";
+    displayTempLowDay3.innerText =
+      weatherData.weatherDayAfterTommorrow.temperatureCLow + "°";
+    displayTempHighDay3.innerText =
+      weatherData.weatherDayAfterTommorrow.temperatureCHigh + "°";
+    displayWeatherConditionDay3.innerText =
+      weatherData.weatherDayAfterTommorrow.condition;
+    displayHumidityDay3.innerText =
+      weatherData.weatherDayAfterTommorrow.humidityAverage;
+    displayWindSpeedDay3.innerText =
+      weatherData.weatherDayAfterTommorrow.windSpeedMaximum + " Km/h";
+    displayUVDay3.innerText = weatherData.weatherDayAfterTommorrow.uv;
+    displaySunriseDay3.innerText = weatherData.weatherDayAfterTommorrow.sunrise;
+    displaySunsetDay3.innerText = weatherData.weatherDayAfterTommorrow.sunset;
   }
 }
